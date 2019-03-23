c8a19ff06db405c1c831d7c42b6e6ab4cabfa3ca
sommersoft <sommersoft@gmail.com>
2019-03-23 09:09:10 -0500

    add Blinka to 'setup.py->install_requires; add PyPI install directions to README

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     author='Adafruit Industries',
     author_email='circuitpython@adafruit.com',
 
-    install_requires=[],
+    install_requires=['Adafruit-Blinka'],
 
     # Choose your license
     license='MIT',
